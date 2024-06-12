8074b6923e7af9134a4d06a447a6cfd764258704
Guy Edwards <guyfedwards@gmail.com>
2024-06-12 16:24:15 +0100

    chore: split out commands/ into smaller files (#89)

---
+++ internal/commands/feeds.go
@@ -0,0 +1,110 @@
+package commands
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/guyfedwards/nom/v2/internal/config"
+	"github.com/guyfedwards/nom/v2/internal/rss"
+	"github.com/guyfedwards/nom/v2/internal/store"
+)
+
+func (c Commands) CleanFeeds() error {
+	urls, err := c.store.GetAllFeedURLs()
+	if err != nil {
+		return fmt.Errorf("[commands.go]: %w", err)
+	}
+
+	var urlsToRemove []string
+
+	for _, u := range urls {
+		inFeeds := false
+		for _, f := range c.config.Feeds {
+			if f.URL == u {
+				inFeeds = true
+			}
+		}
+
+		if !inFeeds {
+			urlsToRemove = append(urlsToRemove, u)
+		}
+	}
+
+	for _, url := range urlsToRemove {
+		err := c.store.DeleteByFeedURL(url, false)
+		if err != nil {
+			return fmt.Errorf("[commands.go]: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (c Commands) GetAllFeeds() ([]store.Item, error) {
+	err := c.CleanFeeds()
+	if err != nil {
+		return []store.Item{}, fmt.Errorf("[commands.go] GetAllFeeds: %w", err)
+	}
+
+	is, err := c.store.GetAllItems()
+	if err != nil {
+		return []store.Item{}, fmt.Errorf("commands.go: GetAllFeeds %w", err)
+	}
+
+	if c.config.ShowFavourites {
+		is = onlyFavourites(is)
+	} else if c.config.ShowRead {
+		is = showRead(is)
+	} else {
+		is = defaultView(is)
+	}
+
+	// add FeedName from config for custom names
+	for i := 0; i < len(is); i++ {
+		for _, f := range c.config.Feeds {
+			if f.URL == is[i].FeedURL {
+				is[i].FeedName = f.Name
+			}
+		}
+	}
+
+	return is, nil
+}
+
+func onlyFavourites(items []store.Item) (is []store.Item) {
+	for _, v := range items {
+		if v.Favourite {
+			is = append(is, v)
+		}
+	}
+
+	return is
+}
+
+// currently showRead shows all items
+func showRead(items []store.Item) (is []store.Item) {
+	return items
+}
+
+func defaultView(items []store.Item) (is []store.Item) {
+	for _, v := range items {
+		if !v.Read() {
+			is = append(is, v)
+		}
+	}
+
+	return is
+}
+
+func fetchFeed(ch chan FetchResultError, wg *sync.WaitGroup, feed config.Feed, version string) {
+	defer wg.Done()
+
+	r, err := rss.Fetch(feed, version)
+
+	if err != nil {
+		ch <- FetchResultError{res: rss.RSS{}, err: err, url: feed.URL}
+		return
+	}
+
+	ch <- FetchResultError{res: r, err: nil, url: feed.URL}
+}
---
+++ internal/commands/filter.go
@@ -0,0 +1,144 @@
+package commands
+
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/sahilm/fuzzy"
+)
+
+// Struct to aid in filtering items into ranks for BubbleTea
+type Filterer struct {
+	FeedNames []string
+	Term      struct {
+		Title     string
+		FeedNames []string
+	}
+}
+
+// Filters by specific filterValue/s on the Filterer.Term
+func (f *Filterer) FilterBy(filterValues []string, targetFilterValues []string, ranks []fuzzy.Match) []fuzzy.Match {
+	if filterValues != nil && len(filterValues) > 0 {
+		var filteredRanks []fuzzy.Match
+		for _, filterValue := range filterValues {
+			for _, rank := range ranks {
+				if strings.ToLower(targetFilterValues[rank.Index]) == filterValue {
+					filteredRanks = append(filteredRanks, rank)
+				}
+			}
+		}
+		return filteredRanks
+	}
+
+	return ranks
+}
+
+// Breaks what's returned from TUIItem.FilterValue() into a TUIItem.
+func (f *Filterer) GetItem(filterValue string) TUIItem {
+	var i TUIItem
+
+	splits := strings.Split(filterValue, "||")
+
+	i.Title = splits[0]
+	i.FeedName = strings.ToLower(splits[1])
+
+	return i
+}
+
+// Extracts `tag:.*` from the stored f.Term.Title
+func (f *Filterer) ExtractFiltersFor(tags ...string) []string {
+	var extractedTags []string
+	done := false
+	for done == false {
+		// `complete` matches 3 potential capture groups after tags, in which
+		// `[^"]` matches a character that isn't a `"`, `[^']` that isn't a `'`,
+		// etc. If it's no quotes, you can also do `feed:with\ spaces`
+		// `incomplete` matches unfinished quoted tags and removes them from the
+		// search. The order of the capture groups MATTERS.
+		// In both examples, the %s section matches all potential tag aliases
+		// passed in for one tag.
+		complete := regexp.MustCompile(fmt.Sprintf(`(%s):("([^"]+)"|'([^']+)'|(([^\\ ]|\\ )+))`, strings.Join(tags, "|")))
+		incomplete := regexp.MustCompile(fmt.Sprintf(`(%s):("[^"]*|'[^']*)`, strings.Join(tags, "|")))
+
+		matches := complete.FindStringSubmatch(f.Term.Title)
+
+		match := ""
+		if matches != nil {
+			// double quotes
+			if matches[3] != "" {
+				match = matches[3]
+				// single quotes
+			} else if matches[4] != "" {
+				match = matches[4]
+				// no quotes
+			} else if matches[5] != "" {
+				match = strings.ReplaceAll(matches[5], `\ `, " ")
+			}
+			f.Term.Title = strings.Replace(f.Term.Title, matches[0], "", 1)
+		} else {
+			// fallback to regular matching without filter
+			matches = incomplete.FindStringSubmatch(f.Term.Title)
+			if matches != nil {
+				f.Term.Title = strings.Replace(f.Term.Title, matches[0], "", 1)
+			}
+			done = true
+		}
+
+		if match != "" {
+			extractedTags = append(extractedTags, strings.ToLower(match))
+		}
+	}
+	if f.Term.Title == "" {
+		f.Term.Title = " "
+	}
+
+	return extractedTags
+}
+
+// Runs all filters
+func (f *Filterer) Filter(targets []string) []fuzzy.Match {
+	var targetTitles []string
+	var targetFeedNames []string
+
+	for _, target := range targets {
+		i := f.GetItem(target)
+		targetTitles = append(targetTitles, i.Title)
+		targetFeedNames = append(targetFeedNames, i.FeedName)
+	}
+
+	ranks := fuzzy.Find(f.Term.Title, targetTitles)
+
+	ranks = f.FilterBy(f.FeedNames, targetFeedNames, ranks)
+
+	sort.Stable(ranks)
+
+	return ranks
+}
+
+func NewFilterer(term string) Filterer {
+	var f Filterer
+
+	f.Term.Title = term
+	f.FeedNames = f.ExtractFiltersFor("feedname", "feed", "f")
+
+	return f
+}
+
+func CustomFilter(term string, targets []string) []list.Rank {
+	filterer := NewFilterer(term)
+
+	ranks := filterer.Filter(targets)
+
+	result := make([]list.Rank, len(ranks))
+	for i, rank := range ranks {
+		result[i] = list.Rank{
+			Index:          rank.Index,
+			MatchedIndexes: rank.MatchedIndexes,
+		}
+	}
+
+	return result
+}
